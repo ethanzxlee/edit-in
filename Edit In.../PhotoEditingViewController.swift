c5bd3294f78a5537c58de59d35f17e775a8c4de3
Zhe Xian Lee (Ethan) <zxlee618@gmail.com>
2018-09-27 08:35:00 +1000

    add logs

--- Edit In.../PhotoEditingViewController.swift
+++ Edit In.../PhotoEditingViewController.swift
@@ -9,20 +9,59 @@
 import Cocoa
 import Photos
 import PhotosUI
-
+import os
 
 class PhotoEditingViewController: NSViewController, PHContentEditingController {
     
     static let adjustmentDataFormatIdentifier = "app.zxlee.edit-in"
     static let adjustmentDataFormatVersion = "1.0.0"
     
+    let cacheDirectoryURL: URL = {
+        let fileManager = FileManager.default
+        let userDefaults = UserDefaultsHelper.groupUserDefaults
+        let useDefaultCachePath = userDefaults.bool(forKey: UserDefaultsHelper.Keys.useDefaultCachePath.rawValue)
+        let customCachePath = userDefaults.string(forKey: UserDefaultsHelper.Keys.customCachePath.rawValue) ?? ""
+        let defaultCacheURL = try! fileManager.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        if useDefaultCachePath && customCachePath == "" {
+            return defaultCacheURL
+        } else {
+            var isDirectory = ObjCBool(true)
+            let exists = fileManager.fileExists(atPath: customCachePath, isDirectory: &isDirectory)
+            let customCacheURL = URL(fileURLWithPath: customCachePath)
+            
+            if !exists && isDirectory.boolValue {
+                do {
+                    try fileManager.createDirectory(at: customCacheURL, withIntermediateDirectories: true, attributes: nil)
+                } catch {
+                    os_log("Could not open the custom cache direcory, will be using the default cache directory", log: OSLog.default, type: .info)
+                    return defaultCacheURL
+                }
+            }
+            return customCacheURL
+        }
+    }()
+    
+    var cachedOriginalImageURL: URL? {
+        if let filename = input.fullSizeImageURL?.lastPathComponent {
+            return cacheDirectoryURL.appendingPathComponent(filename)
+        }
+        os_log("Tried to get cachedOriginalImageURL when input is nil", log: OSLog.default, type: .info)
+        return nil
+    }
+    
+    var input: PHContentEditingInput!
+    var importURL: URL?
+    
+    @IBOutlet weak var importedImageTextField: NSTextField!
+    @IBOutlet weak var importImageButton: NSButton!
+    @IBOutlet weak var applicationsPopUpButton: NSPopUpButton!
+    @IBOutlet weak var compareButton: NSButton!
     @IBOutlet weak var imageView: NSImageView!
     @IBOutlet weak var imageScrollView: NSScrollView!
     @IBOutlet weak var clipView: CenteringClipView!
     @IBOutlet weak var documentViewWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var documentViewHeightConstraint: NSLayoutConstraint!
-    var input: PHContentEditingInput!
-    var importURL: URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +82,8 @@ class PhotoEditingViewController: NSViewController, PHContentEditingController {
             documentViewWidthConstraint.constant = inputImage.size.width
             documentViewHeightConstraint.constant = inputImage.size.height
             imageScrollView.magnify(toFit: NSRect(x: 0, y: 0, width: inputImage.size.width, height: inputImage.size.height))
+        } else {
+            imageView.image = nil
         }
     }
     
@@ -62,17 +103,17 @@ class PhotoEditingViewController: NSViewController, PHContentEditingController {
     
     func cancelContentEditing() {
         let fileManager = FileManager.default
-        
         do {
-        for filePath in try fileManager.contentsOfDirectory(atPath: cacheURL.path) {
-            do {
-                try fileManager.removeItem(atPath: filePath)
-            } catch {
-                print("Failed to remove: \(error)")
+            for filePath in try fileManager.contentsOfDirectory(atPath: cacheDirectoryURL.path) {
+                do {
+                    let absoluteFilePath = cacheDirectoryURL.appendingPathComponent(filePath).path
+                    try fileManager.removeItem(atPath: absoluteFilePath)
+                } catch {
+                    os_log("Failed to remove item when clearing cache", log: OSLog.default, type: .info)
+                }
             }
-        }
         } catch {
-            print("could not list cache dir")
+            os_log("Failed to list contents of the cache directory when clearing cache", log: OSLog.default, type: .info)
         }
     }
     
@@ -86,63 +127,50 @@ class PhotoEditingViewController: NSViewController, PHContentEditingController {
             } catch {
                 // TODO: Display error
                 print(error)
+                os_log("Failed to open image", log: OSLog.default, type: .error)
             }
         } else {
-            // TODO: Display error
-            print("Full size image url is nil")
+            os_log("Tried to open image when input fullSizeImageURL is nil", log: OSLog.default, type: .error)
         }
+    }
+    
+    @IBAction func handleImportEditedImage(_ sender: Any) {
+        importEditedPhoto()
     }
     
     // MARK: - Methods
     
-    var cacheURL: URL {
-        let fileManager = FileManager.default
-        let userDefaults = UserDefaultsHelper.groupUserDefaults
-        let useDefaultCachePath = userDefaults.bool(forKey: UserDefaultsHelper.Keys.useDefaultCachePath.rawValue)
-        let customCachePath = userDefaults.string(forKey: UserDefaultsHelper.Keys.customCachePath.rawValue) ?? ""
-        let defaultCacheURL = try! fileManager.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        if useDefaultCachePath && customCachePath == "" {
-            return defaultCacheURL
-        }
-        else {
-            var isDirectory = ObjCBool(true)
-            let exists = fileManager.fileExists(atPath: customCachePath, isDirectory: &isDirectory)
-            let customCacheURL = URL(fileURLWithPath: customCachePath)
-            
-            if !exists && isDirectory.boolValue {
-                do {
-                    try fileManager.createDirectory(at: customCacheURL, withIntermediateDirectories: true, attributes: nil)
-                } catch {
-                    // TODO: Display error
-                    print("Error creating custom cache directory")
-                    return defaultCacheURL
-                }
-            }
-            return customCacheURL
-        }
-    }
-    
-    var cacheImageURL: URL {
-        return cacheURL.appendingPathComponent(input.fullSizeImageURL!.lastPathComponent)
-    }
-    
     func copyImageToCache() throws {
+        guard let cachedOriginalImageURL = cachedOriginalImageURL else {
+            os_log("Could not get cachedOriginalImageURL when copying image to cache directory", log: OSLog.default, type: .info)
+            return
+        }
+        
         let fileManager = FileManager.default
-        if !fileManager.fileExists(atPath: cacheImageURL.path) {
-            try FileManager.default.copyItem(at: input.fullSizeImageURL!, to: cacheImageURL)
+        if !fileManager.fileExists(atPath: cachedOriginalImageURL.path) {
+            try FileManager.default.copyItem(at: input.fullSizeImageURL!, to: cachedOriginalImageURL)
         }
     }
     
     func openImageExternally() {
+        guard let cachedOriginalImageURL = cachedOriginalImageURL else {
+            os_log("Could not get cachedOriginalImageURL when opening image", log: OSLog.default, type: .info)
+            return
+        }
+        
         let userDefaults = UserDefaultsHelper.groupUserDefaults
         let preferredApplicationPath = userDefaults.string(forKey: UserDefaultsHelper.Keys.preferredApplicationPath.rawValue)!
-        NSWorkspace.shared.openFile(cacheImageURL.path, withApplication: URL(fileURLWithPath: preferredApplicationPath).path)
+        let openSuccess = NSWorkspace.shared.openFile(cachedOriginalImageURL.path, withApplication: URL(fileURLWithPath: preferredApplicationPath).path)
+        if !openSuccess {
+            // TODO: display error message
+            os_log("Could not open image with selected application", log: .default, type: .error)
+        }
     }
     
-    func importPhoto() {
+    func importEditedPhoto() {
         // Show OpenFilePanel
         // get the url into importURL
+        
     }
     
 }
